41d3c94993410c0d41e9754eff6e2ca7bdf981c3
Daniel T. Lee <danieltimlee@gmail.com>
2017-06-15 04:08:23 +0900

    FDS module Enabled

--- src/main.c
+++ src/main.c
@@ -74,8 +74,9 @@
 
 #define DEAD_BEEF                       0xDEADBEEF                                  /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
-
 //#define FINAL 1
+
+static volatile uint8_t write_flag=0;
 
 static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID;                            /**< Handle of the current connection. */
 
@@ -892,6 +893,113 @@ void Button_Click_CallBack() {
 }
 
 
+#define APP_FILE_ID     0x1111
+#define APP_REC_KEY     0x2222
+
+static void app_fds_evt_handler(fds_evt_t const *const p_fds_evt) {
+    switch (p_fds_evt->id) {
+        case FDS_EVT_INIT:
+            if (p_fds_evt->result != FDS_SUCCESS) {
+                LOG_D("FDS Init Failed!!\r\n");
+            }
+            break;
+        case FDS_EVT_WRITE:
+            if (p_fds_evt->result == FDS_SUCCESS) {
+                write_flag = 1;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+static ret_code_t app_fds_write(void) {
+    static uint32_t const m_deadbeef[2] = {0xDEADBEEF, 0xBAADF00D};
+    fds_record_t record;
+    fds_record_desc_t record_desc;
+    fds_record_chunk_t record_chunk;
+    // Set up data.
+    record_chunk.p_data = m_deadbeef;
+    record_chunk.length_words = 2;
+    // Set up record.
+    record.file_id = APP_FILE_ID;
+    record.key = APP_REC_KEY;
+    record.data.p_chunks = &record_chunk;
+    record.data.num_chunks = 1;
+
+    ret_code_t ret = fds_record_write(&record_desc, &record);
+    if (ret != FDS_SUCCESS) {
+        return ret;
+    }
+    LOG_D("Writing Record ID = %d \r\n", record_desc.record_id);
+    return NRF_SUCCESS;
+}
+
+static ret_code_t fds_read(void) {
+    fds_flash_record_t flash_record;
+    fds_record_desc_t record_desc;
+    fds_find_token_t ftok = {0};//Important, make sure you zero init the ftok token
+    uint32_t * data;
+    uint32_t err_code;
+
+    LOG_D("Start searching... \r\n");
+    // Loop until all records with the given key and file ID have been found.
+    while (fds_record_find(APP_FILE_ID, APP_REC_KEY, &record_desc, &ftok) == FDS_SUCCESS) {
+        err_code = fds_record_open(&record_desc, &flash_record);
+        if (err_code != FDS_SUCCESS) {
+            return err_code;
+        }
+
+        LOG_D("Found Record ID = %d\r\n", record_desc.record_id);
+        LOG_D("Data = ");
+        data = (uint32_t *) flash_record.p_data;
+        for (uint8_t i = 0; i < flash_record.p_header->tl.length_words; i++) {
+            LOG_D("0x%8x ", data[i]);
+        }
+        LOG_D("\r\n");
+        // Access the record through the flash_record structure.
+        // Close the record when done.
+        err_code = fds_record_close(&record_desc);
+        if (err_code != FDS_SUCCESS) {
+            return err_code;
+        }
+    }
+    return NRF_SUCCESS;
+}
+
+static ret_code_t app_fds_find_and_delete(void) {
+    fds_record_desc_t record_desc;
+    fds_find_token_t ftok;
+
+    ftok.page = 0;
+    ftok.p_addr = NULL;
+    // Loop and find records with same ID and rec key and mark them as deleted.
+    while (fds_record_find(APP_FILE_ID, APP_REC_KEY, &record_desc, &ftok) == FDS_SUCCESS) {
+        fds_record_delete(&record_desc);
+        LOG_D("Deleted record ID: %d \r\n", record_desc.record_id);
+    }
+    // call the garbage collector to empty them, don't need to do this all the time, this is just for demonstration
+    ret_code_t ret = fds_gc();
+    if (ret != FDS_SUCCESS) {
+        return ret;
+    }
+    return NRF_SUCCESS;
+}
+
+static ret_code_t app_fds_init(void) {
+    ret_code_t ret = fds_register(app_fds_evt_handler);
+    if (ret != FDS_SUCCESS) {
+        return ret;
+    }
+    ret = fds_init();
+    if (ret != FDS_SUCCESS) {
+        return ret;
+    }
+
+    return NRF_SUCCESS;
+}
+
+
 /**@brief Function for application main entry.
  */
 int main(void) {
@@ -923,6 +1031,16 @@ int main(void) {
     services_init();
     advertising_init();
     conn_params_init();
+
+    err_code =app_fds_init();
+    APP_ERROR_CHECK(err_code);
+    err_code = app_fds_find_and_delete();
+    APP_ERROR_CHECK(err_code);
+    err_code =app_fds_write();
+    APP_ERROR_CHECK(err_code);
+    //wait until the write is finished. 
+    while (write_flag==0);
+    fds_read();
 
     LOG_D("Ram Size : %d kb \r\n", sizeof(APP) / 1024);
     LOG_D("%s Addr : %s\r\n", LOG_PUSH(APP.dev.name), STR_PUSH(APP.dev.my_addr.addr, 1));
